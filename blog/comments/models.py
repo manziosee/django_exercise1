1791b97a771b6db1e3f4e3af2dd7c4d89541a70d
manzi osee <oseemanzi3@gmail.com>
2022-06-29 21:48:10 +0200

    commit

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tomlkit import comment
 from myblog.models import Blog
 from django.contrib.auth import get_user_model
 user=get_user_model()
@@ -6,5 +7,8 @@ import uuid
 
 # Create your models here.
 class Comment(models.Model):
-    id=models.UUIDField(default=uuid.uuid4,primary_key=True,editable=True)
+    id=models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
+    comment_by=models.ForeignKey(user,related_name="user_comment", on_delete=models.CASCADE)
+    comment_content=models.TextField()
+    created_on=models.DateField(auto_now_add=True)
     
